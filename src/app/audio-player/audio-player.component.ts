d9c3bb12eafa255108c2df2ace89297d1aad90ee
Joel LaFever <lafeverjoel@gmail.com>
2026-03-25 10:09:39 -0400

    added inital styling for the major components

--- src/app/audio-player/audio-player.component.ts
+++ src/app/audio-player/audio-player.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { Song } from '../shared/types';
 import { FileUploadComponent } from './file-upload/file-upload.component';
@@ -10,32 +11,51 @@ import { FileUploadComponent } from './file-upload/file-upload.component';
     templateUrl: './audio-player.component.html',
     styleUrls: ['./audio-player.component.scss'],
     standalone: true,
-    imports: [CommonModule, MatIconModule, FileUploadComponent]
+    imports: [CommonModule, MatIconModule, MatButtonModule, FileUploadComponent]
 })
 export class AudioPlayerComponent implements OnInit {
     @ViewChild('player') player!: ElementRef<HTMLAudioElement>;
 
     fileName = '';
+    fileUrl = '';
     songs: Song[] = [];
+    isPlaying = false;
+    isMuted = false;
 
     constructor(private http: HttpClient) { }
 
     ngOnInit(): void {
     }
 
-    play() {
-  this.player.nativeElement.play();
-}
+    togglePlay() {
+      const audio = this.player.nativeElement;
+      if (audio.paused) {
+        audio.play();
+        this.isPlaying = true;
+      } else {
+        audio.pause();
+        this.isPlaying = false;
+      }
+    }
+
+    toggleMute() {
+      const audio = this.player.nativeElement;
+      this.isMuted = !this.isMuted;
+      audio.muted = this.isMuted;
+    }
 
     playSong(song: Song) {
-  this.player.nativeElement.src = song.url;
-  this.fileName = song.name;
-  this.player.nativeElement.play();
-}
+      this.fileUrl = song.url;
+      this.fileName = song.name;
+      this.songs.push(song);
+      this.player.nativeElement.src = song.url;
+      this.player.nativeElement.play();
+      this.isPlaying = true;
+    }
 
     onFileSelected(song: Song) {
-        this.fileName = song.name;
-        this.player.nativeElement.src = song.url;
-        this.songs.push(song);
+      this.fileUrl = song.url;
+      this.fileName = song.name;
+      this.songs.push(song);
     }
 }
